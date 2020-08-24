a14b85ad79754e7fa390b34b0bb08a2b7b9c08c3
Steven Huang <v-jianch@microsoft.com>
2020-08-24 10:08:35 +0000

    update

--- ConsoleApp1ss/Program.cs
+++ ConsoleApp1ss/Program.cs
@@ -1,12 +1,66 @@
 ﻿using System;
-
-namespace ConsoleApp1ss
+namespace SimpleEvent
 {
-    class Program
+    using System;
+    public class EventTest
     {
-        static void Main(string[] args)
+        private int value;
+
+        public delegate void NumManipulationHandler();
+
+
+        public event NumManipulationHandler ChangeNum;
+        protected virtual void OnNumChanged()
         {
-            Console.WriteLine("Hello World!");
+            if (ChangeNum != null)
+            {
+                ChangeNum(); 
+            }
+            else
+            {
+                Console.WriteLine("event not fire");
+                Console.ReadKey(); 
+            }
+        }
+
+
+        public EventTest()
+        {
+            int n = 5;
+            SetValue(n);
+        }
+
+
+        public void SetValue(int n)
+        {
+            if (value != n)
+            {
+                value = n;
+                OnNumChanged();
+            }
+        }
+    }
+
+
+
+    public class subscribEvent
+    {
+        public void printf()
+        {
+            Console.WriteLine("event fire");
+            Console.ReadKey();
+        }
+    }
+
+    public class MainClass
+    {
+        public static void Main()
+        {
+            EventTest e = new EventTest(); 
+            subscribEvent v = new subscribEvent(); 
+            e.ChangeNum += new EventTest.NumManipulationHandler(v.printf); 
+            e.SetValue(7);
+            e.SetValue(11);
         }
     }
 }
